00e32b18a311d2d6ff6582bfad8917547b7b9267
TanbirHasan <mdtanbir576@gmail.com>
2023-05-28 22:15:08 +0600

    coureses and auth functionality updated

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ const mongoose = require("mongoose");
 const mediaRoutes = require('./routes/media')
 const registerRoutes = require('./routes/register')
 const authRoutes = require('./routes/auth')
-const refreshRoutes = require('./routes/refresh')
+const refreshRoutes = require('./routes/refresh');
+const corsOptions = require("./config/corsOptions");
+const credentials = require("./middlewares/credentials");
 
 // middlewares
 
 app.use(express.json());
-app.use(cors());
+app.use(credentials);
+app.use(cors(corsOptions));
 
 app.use('/api/v1/media',mediaRoutes)
-app.use('/api/v1/public',express.static(path.join(__dirname, 'public')))
+
 app.use('/api/v1/register',registerRoutes );
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/refresh', refreshRoutes);
+app.use('/public',express.static(path.join(__dirname, 'public')))
 
 
 
